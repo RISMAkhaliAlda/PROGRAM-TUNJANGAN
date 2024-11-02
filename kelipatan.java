b05668a3cee5e74f0600b8160014b3072db9a9c8
RISMAkhaliAlda <rismakhalida@gmail.com>
2024-11-02 14:53:38 +0700

    Add files via upload

---
+++ kelipatan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author COR i5 GEN 4
+ */
+public class kelipatan {
+    public static void main(String[] args) {
+        double angka = 3.5;
+        while (angka <= 35){
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+}
+    
+  
+
